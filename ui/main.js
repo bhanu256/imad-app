e6fba54c82d523dc268b758e6abafea7d50a1897
bhanu256 <bhanuphani100@gmail.com>
2017-08-16 20:10:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button=document.getElementById("count");
+var counter=0;
+
+button.onclick=function(){
+    counter=counter+1;
+    counter.innerHTML=counter.toString();
+}
